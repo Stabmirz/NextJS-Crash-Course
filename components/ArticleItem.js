9118486b5a89f5fbae979ec97e12d9b32d80fb19
Abi Mirza <amirza@techlaunch.io>
2021-06-27 22:37:03 -0400

    first commit

---
+++ components/ArticleItem.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import articleStyles from '../styles/Article.module.css'
+
+import React from 'react'
+
+const ArticleItems = ({article}) => {
+    return (
+        <Link href="/article/[id]" as={`/article/${article.id}`}>
+            <a className={articleStyles.card} >
+                <h3>{article.title} &rarr;</h3>
+                <p>{article.excerpt}</p>
+            </a>
+            
+        </Link>
+    )
+}
+
+export default ArticleItems
